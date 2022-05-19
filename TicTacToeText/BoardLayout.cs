1e1ac5dfd28e3bc6f685b3bbb98d39ad59cb85a8
MrChri5 <chrisjweaver@outlook.com>
2022-05-19 06:26:52 +0100

    Add project files.

---
+++ TicTacToeText/BoardLayout.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeText
+{
+    class BoardLayout
+    {
+
+        //define board
+        // layout  for back end analysis
+        // display layout for text based input and output
+        // both single and double indexing of grid
+        // get each column/row/diag and index all in collection
+        // get row/col/diag status (own set-up, opp set-up, contested, own single, opp single, empty) 
+
+        private static int size;
+        private static int maxSize = 9;
+        private string[,] layout = new string[maxSize, maxSize];
+        private string [] displayLayout = new string[maxSize * maxSize];
+        private List<string[]> currentLines = new List<string[]>();
+        private StatusType[] currentStatus;
+        private string ownPiece = "X";
+        private string oppPiece = "O";
+        private string errorPiece = "e";
+
+        public BoardLayout()
+        {
+            //default grid size is 3x3
+            BoardInit(3);
+        }
+        public BoardLayout(int x)
+        {
+            if (x > maxSize)
+            {
+                x = maxSize;
+            }
+            BoardInit(x);
+        }
+        public void BoardInit(int x)
+        {
+            //initialize board to numbered cells left to right, top to bottom
+            size = x;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    int k = 1 + j + size * i;
+                    layout[i, j] = k.ToString();
+                    displayLayout[k - 1] = k.ToString();
+                }
+            }
+            currentLines = GetLines(layout);
+            currentStatus = GetStatus(currentLines);
+        }
+
+        public int Size
+        {
+            get { return size; }
+        }
+
+        public void SetOwn(int x)
+        {
+            //set own piece X
+            int i = Math.DivRem(x-1, size, out int j);
+            SetOwn(i , j);
+        }
+        public void SetOwn(int i, int j)
+        {
+            int k = j + size * i;
+            if (!validBlank(layout[i, j]))
+            {
+                layout[i, j] = errorPiece;
+                displayLayout[k] = errorPiece;
+                return;
+            }
+            layout[i, j] = ownPiece;
+            displayLayout[k] = ownPiece;
+            currentLines = GetLines(layout);
+            currentStatus = GetStatus(currentLines);
+        }
+
+        public void SetOpp(int x)
+        {
+            //set opponant piece O
+            int i = Math.DivRem(x-1, size, out int j);
+            SetOpp(i , j);
+        }
+        public void SetOpp(int i, int j)
+        {
+            int k = j + size * i;
+            if (!validBlank(layout[i, j]))
+            {
+                layout[i, j] = errorPiece;
+                displayLayout[k] = errorPiece;
+                return;
+            }
+            layout[i, j] = oppPiece;
+            displayLayout[k] = oppPiece;
+            currentLines = GetLines(layout);
+            currentStatus = GetStatus(currentLines);
+        }
+
+        public string[,] Layout
+        {
+            get { return layout; }
+        }
+
+        public string[] DisplayLayout
+        {
+            get { return displayLayout; }
+        }
+
+        public List<string[]> Lines
+        {
+            get { return currentLines; }
+        }
+
+        public StatusType[] Status
+        {
+            get { return currentStatus; }
+        }
+
+        public bool validBlank(int k)
+            //check if the kth position on the board is available
+            //if it has already been played then its entry in the display layout will be an 'X' or 'O'
+        {
+            if (k > 0 && k <= size * size)
+            {
+                return validBlank(displayLayout[k - 1]);
+            }
+            return false;
+        }
+
+        public bool validBlank(string x)
+        {
+            //check space is not taken, available spaces will contain numbers. spaces are filled with 'X' or 'O' when taken
+            if (!int.TryParse(x,out int i))
+            {
+                return false;
+            }
+            if(i>0 && i <= size * size)
+            {
+                return true;
+            }
+            return false;    
+        }
+
+        private List<string[]> GetLines(string[,] getLinesLayout)
+        {
+            List<string[]> LinesList = new List<string[]>();
+            //take layout and produce a list of lines
+            string[] line = new string[size];
+            LinesList.Clear();
+            //rows
+            for (int i = 0; i < size; i++)
+            {
+                line = new string[size];
+                for (int j = 0; j < size; j++)
+                {
+                    line[j] = getLinesLayout[i, j];
+                }
+                LinesList.Add(line);
+            }
+            //columns
+            for (int i = 0; i < size; i++)
+            {
+                line = new string[size];
+                for (int j = 0; j < size; j++)
+                {
+                    line[j] = getLinesLayout[j, i];
+                }
+                LinesList.Add(line);
+            }
+            //diagonals
+            line = new string[size];
+            for (int i = 0; i < size; i++)
+            {
+                line[i] = getLinesLayout[i, i];
+            }
+            LinesList.Add(line);
+            line = new string[size];
+            for (int i = 0; i < size; i++)
+            {
+                line[i] = getLinesLayout[size - i - 1, i];
+            }
+            LinesList.Add(line);
+            return LinesList;
+        }
+        
+        public enum StatusType { own, opp, ownSetup, oppSetup, ownSetupFork, oppSetupFork, contested, ownOther, oppOther, empty, other, error};
+
+        private StatusType[] GetStatus (string[,] statusLayout)
+        {
+            return GetStatus(GetLines(statusLayout));
+        }
+        private StatusType[] GetStatus (List<string[]> checkLines)
+        {
+            //analyse lines 
+            StatusType[] status = new StatusType[checkLines.Count];
+            int lineNum = 0;
+              
+            foreach(string [] line in checkLines)
+            {
+                int own = 0;
+                int opp = 0;
+                bool errorFlag = false;
+                foreach (string piece in line)
+                {
+                    if (piece == errorPiece)
+                    {
+                    errorFlag = true;
+                    }
+                    if (piece == ownPiece)
+                    {
+                        ++own;
+                    }
+                    if (piece == oppPiece)
+                    {
+                        ++opp;
+                    }
+                }
+                if (errorFlag || own + opp > size )
+                {
+                    status[lineNum] = StatusType.error;
+                }
+                else if (own == size)
+                {
+                    status[lineNum] = StatusType.own;
+                }
+                else if ( opp == size)
+                {
+                    status[lineNum] = StatusType.opp;
+                }
+                else if (own == 0 && opp == 0)
+                {
+                    status[lineNum] = StatusType.empty;
+                }
+                else if (own == 0 && opp == size -1 )
+                {
+                    status[lineNum] = StatusType.oppSetup;
+                }
+                else if (own == 0 && opp == size - 2)
+                {
+                    status[lineNum] = StatusType.oppSetupFork;
+                }
+                else if (own == 0 && opp > 0)
+                {
+                    status[lineNum] = StatusType.oppOther;
+                }
+                else if (opp == 0 && own == size - 1)
+                {
+                    status[lineNum] = StatusType.ownSetup;
+                }
+                else if (opp == 0 && own == size - 2)
+                {
+                    status[lineNum] = StatusType.ownSetupFork;
+                }
+                else if (opp == 0 && own > 0)
+                {
+                    status[lineNum] = StatusType.ownOther;
+                }
+                else if (opp > 0 && own > 0)
+                {
+                    status[lineNum] = StatusType.contested;
+                }
+                else
+                {
+                    status[lineNum] = StatusType.other;
+                }
+                    ++lineNum;
+            }
+            return status;
+        }                
+    }
+}
---
+++ TicTacToeText/Program.cs
@@ -0,0 +1,466 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeText
+{
+    class Program
+    {
+        static bool twoPlayer = false;
+        static bool aiFirst = true;
+        static int boardSize = 3;
+        static string gameLog = "v1";
+
+        static void Main(string[] args)
+        {           
+            BoardLayout currentBoard = new BoardLayout(boardSize);
+            bool playerTurn = true;
+            bool restart = true;
+            bool settings = false;
+            bool turnSwitch = false;
+            string input = "";
+            Console.WriteLine("Choose mode:");
+            Console.WriteLine("1 - Two Player");
+            Console.WriteLine("2 - AI Second");
+            Console.WriteLine("3 - AI First");
+            Console.WriteLine("4 - AI Alternate");
+
+            while ((input = Console.ReadLine()) != null && input != "exit")
+            {
+                if (!settings && input!="")
+                {
+                     //first time in game give option to change custom settings
+                    switch (input[0])
+                    {
+                        case '1':
+                            //two player game
+                            twoPlayer = true;
+                            
+                            break;
+                        case '2':
+                            //human vs ai - human always first
+                            twoPlayer = false;
+                            aiFirst = false;
+                            break;
+                        case '3':
+                            // human vs ai - ai always first
+                            twoPlayer = false;
+                            aiFirst = true;
+                            break;
+                        case '4':
+                            // human vs ai - alternate
+                            twoPlayer = false;
+                            aiFirst = false;
+                            turnSwitch = true;
+                            break;
+                        default:
+                            break;
+                    }
+                    settings = true;
+                }
+                settings = true;
+                
+                if (restart)                   
+                // if restart flag is set, initialize the board
+                {
+                    currentBoard = new BoardLayout(boardSize);
+                    playerTurn = true;
+                    restart = false;
+                    gameLog = "v1" + DateTime.Now.ToString();
+                    DisplayBoard(currentBoard);
+                    //alternate who goes first if set to alternate 
+                    if (turnSwitch)
+                    {
+                        aiFirst = !aiFirst;
+                    }
+
+                    //if restarting and AI is set to go first, play the AI's first turn
+                    if (aiFirst && !twoPlayer)
+                    {
+                        int moveAI = AIOppMove(currentBoard);
+                        Console.WriteLine("AI played position " + moveAI);
+                        Console.WriteLine();
+                        if (!currentBoard.validBlank(moveAI))
+                        {
+                            Console.WriteLine("AI error");
+                            gameLog += "AIerror";
+                            SaveLog();
+                            restart = true;
+                            continue;
+                        }
+                        gameLog += moveAI.ToString();
+                        currentBoard.SetOpp(moveAI);
+                        DisplayBoard(currentBoard);
+                        Console.WriteLine("Your move");
+                    }                 
+                    continue;
+                }
+
+                if (int.TryParse(input, out int inputNum) && inputNum <= boardSize * boardSize && inputNum > 0)
+                {
+                    if (currentBoard.validBlank(inputNum))
+                    {
+                        if (twoPlayer)
+                        {
+                            if (playerTurn)
+                            {
+                                currentBoard.SetOwn(inputNum);
+                                playerTurn = false;
+                            }
+                            else
+                            {
+                                currentBoard.SetOpp(inputNum);
+                                playerTurn = true;
+                            }
+                            DisplayBoard(currentBoard);
+                            if (playerTurn)
+                            {
+                                Console.WriteLine("Player X's turn");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Player O's turn");
+                            }
+                            restart = CheckGameEnd(currentBoard);
+                        }
+                        if (!twoPlayer)
+                        {
+                            gameLog += "H" + inputNum.ToString();
+                            currentBoard.SetOwn(inputNum);
+                            DisplayBoard(currentBoard);
+                            restart = CheckGameEnd(currentBoard);
+                            if (!restart)
+                            {
+                                int moveAI = AIOppMove(currentBoard);
+                                Console.WriteLine("AI played position " + moveAI);
+                                Console.WriteLine();
+                                if (!currentBoard.validBlank(moveAI))
+                                {
+                                    Console.WriteLine("AI error");
+                                    gameLog += "AIerror";
+                                    SaveLog();
+                                    restart = true;
+                                    continue;
+                                }
+                                gameLog += moveAI.ToString();
+                                currentBoard.SetOpp(moveAI);
+                                DisplayBoard(currentBoard);
+                                restart = CheckGameEnd(currentBoard);
+                                if (!restart)
+                                {
+                                    Console.WriteLine("Your turn");
+                                }
+                            }
+                        }
+                        if (restart)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Press ENTER to start a new game.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Space already taken.");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Not valid input");
+                }
+            }
+            gameLog += "Exit";
+            SaveLog();
+        }
+
+        static bool CheckGameEnd(BoardLayout currentBoard)
+        {
+            string oppWinMsg = "O Wins";
+            string ownWinMsg = "X Wins";
+            string drawMsg = "You Drew";
+
+            if (!twoPlayer)
+            {
+                ownWinMsg = "You Win";
+                oppWinMsg = "I Win!! =)";
+                drawMsg = "We Drew";
+            }
+            for (int i = 0; i < currentBoard.Status.Length; i++)
+            {
+                switch (currentBoard.Status[i])
+                {
+                    case BoardLayout.StatusType.opp:
+                        Console.WriteLine(oppWinMsg);
+                        gameLog += "AIWin";
+                        SaveLog();
+                        return true;
+                    case BoardLayout.StatusType.own:
+                        Console.WriteLine(ownWinMsg);
+                        gameLog += "HWin";
+                        SaveLog();
+                        return true;
+                    default:
+                        break;
+                }
+            }
+            // check if there are any free spaces left
+            for (int j=1; j <= currentBoard.Size * currentBoard.Size; j++)
+            {
+                if (currentBoard.validBlank(j))
+                {
+                    return false;
+                }
+            }
+            Console.WriteLine(drawMsg);
+            gameLog += "Draw";
+            SaveLog();
+            return true;
+        }
+
+        static void DisplayBoard(BoardLayout currentBoard)
+        {
+            int cellLength = (currentBoard.Size * currentBoard.Size).ToString().Length;
+            char gridVer = '│';
+            char gridHor = '─';
+            char gridInt = '┼';
+            string gridLine = "";
+
+            for (int i = 0; i < currentBoard.Size; i++)
+            {
+                gridLine += "".PadLeft(cellLength, gridHor);
+                if (i < currentBoard.Size - 1)
+                {
+                    gridLine += gridInt;
+                }
+            }
+
+            for (int i = 0; i < currentBoard.Size; i++)
+            {
+                string printLine = "";
+                for (int j = 0; j < currentBoard.Size; j++)
+                {
+                    printLine += currentBoard.Layout[i, j].PadLeft(cellLength,'0');
+                    if (j < currentBoard.Size - 1)
+                    {
+                        printLine += gridVer;
+                    }
+                }
+                Console.WriteLine(printLine);
+                if (i < currentBoard.Size - 1)
+                {
+                    Console.WriteLine(gridLine);
+                }
+            }
+            Console.WriteLine("");
+        }
+
+        static int AIOppMove(BoardLayout currentBoard)
+        {
+            //compute the AI (opponent) move
+            // analyse each column/row/diag in turn, check for:
+            //1) can get 3 in a row
+            //2) opponent can get 3 , and should block            
+            //3) can create fork
+            //4) can block opponent creation of fork
+            //5) prepare fork (check #7 for preferance)
+            //6) any empty row
+            //7) set up 3 in a row
+            //8) go anywhere
+
+            int winAI = -1;
+            int blockWin = -1;
+            int[] setupFork = new int[currentBoard.Size * currentBoard.Status.Length];
+            bool setupForkYes = false;
+            int[] blockFork = new int[currentBoard.Size * currentBoard.Status.Length];
+            int[] prepFork = new int[currentBoard.Size * currentBoard.Status.Length];
+            bool prepForkYes = false;
+            int[] prepForkLine = new int[currentBoard.Size * currentBoard.Status.Length];
+            int setupForkPtr = 0;
+            int blockForkPtr = 0;
+            int prepForkPtr = 0;
+
+            gameLog += "AI";
+                
+            gameLog+="s";
+            for (int i = 0; i < currentBoard.Status.Length; i++)
+            {
+                gameLog += ((int)currentBoard.Status[i]).ToString();
+                switch (currentBoard.Status[i])
+                {
+                    //if a line is already completed return -1 as an error
+                    case BoardLayout.StatusType.own:
+                    case BoardLayout.StatusType.opp:
+                    case BoardLayout.StatusType.error:
+                        return -1;
+                    //if AI can complete line, get index of line to complete
+                    case BoardLayout.StatusType.oppSetup:
+                        winAI = i;
+                        break;
+                    // if human player can get win, block this
+                    case BoardLayout.StatusType.ownSetup:
+                        blockWin = i;
+                        break;
+                    // find indexes of free spaces in following lines
+                    case BoardLayout.StatusType.oppSetupFork:
+                    case BoardLayout.StatusType.ownSetupFork:
+                    case BoardLayout.StatusType.empty:
+                    case BoardLayout.StatusType.oppOther:
+                    string[] forkLine = currentBoard.Lines[i];
+
+                        for (int j = 0; j < currentBoard.Size; j++)
+                        {
+                            if (currentBoard.validBlank(forkLine[j]))
+                            {
+                                //space to setup fork
+                                if (currentBoard.Status[i] == BoardLayout.StatusType.oppSetupFork)
+                                {
+                                    setupFork[setupForkPtr] = int.Parse(forkLine[j]);
+                                    setupForkPtr++;
+                                    setupForkYes = true;
+                                }
+                                //space to block setup fork
+                                else if (currentBoard.Status[i] == BoardLayout.StatusType.ownSetupFork)
+                                {
+                                    blockFork[blockForkPtr] = int.Parse(forkLine[j]);
+                                    blockForkPtr++;
+                                }
+                                //spaces and line numbers to prepare forks to setup next turn
+                                else
+                                {
+                                    prepFork[prepForkPtr] = int.Parse(forkLine[j]);
+                                    prepForkLine[prepForkPtr] = i;
+                                    prepForkPtr++;
+                                    prepForkYes = true;
+                                }
+                            }
+                        }
+                        break;  
+                      
+                    default:
+                        break;
+                }
+            }
+            gameLog += "s";
+
+            // if either player can complete a line, get the index of the move to win or block
+            if (winAI != -1 || blockWin != -1)
+            {
+                gameLog += "a";
+                int i = blockWin;
+                if (winAI != -1)
+                {
+                    gameLog += "b";
+                    i = winAI;
+                }
+                string[] winLine = currentBoard.Lines[i];
+                for (int j = 0; j < currentBoard.Size; j++)
+                {
+                    if (currentBoard.validBlank(winLine[j]))
+                    {
+                        return int.Parse(winLine[j]);
+                    }
+                }
+            }
+
+            // check for duplicates in setupFork. duplicate entries means we can create a fork
+            for (int i = 0; i < setupForkPtr; i++)
+            {
+                for (int j = 0; j < setupForkPtr; j++)
+                {
+                    if (i != j && setupFork[i] == setupFork[j])
+                    {
+                        gameLog += "c";
+                        return setupFork[i];
+                    }
+                }
+            }
+
+            // similarly, check blockFork to block player fork.
+            for (int i = 0; i < blockForkPtr; i++)
+            {
+                for (int j = 0; j < blockForkPtr; j++)
+                {
+                    if (i != j && blockFork[i] == blockFork[j])
+                    {
+                        gameLog += "d";
+                        return blockFork[i];
+                    }
+                }
+            }
+
+            //prepare fork to setup next turn
+            int prepForkNum = 0;
+            for (int i =0; i < setupForkPtr; i++)
+            {
+                for (int j =0; j< prepForkPtr; j++)
+                {
+                    if(setupFork[i] == prepFork[j])
+                    {
+                        for (int k = 0; k < currentBoard.Size; k++)
+                        {
+                            // if move also sets up a line, choose it now other wise store it for later
+                            if (int.TryParse(currentBoard.Lines[prepForkLine[j]][k], out prepForkNum) && 
+                                prepForkNum != setupFork[i] && 
+                                setupFork.Contains<int>(prepForkNum) && 
+                                currentBoard.validBlank(prepForkNum))
+                            {
+                                gameLog += "f";
+                                return prepForkNum;                    
+                            }
+                        }
+                    }
+                }
+            }
+            //if a fork was prepared, choose it.
+            if (currentBoard.validBlank(prepForkNum))
+            {
+                gameLog += "g";
+                return prepForkNum;
+            }
+
+            // else go in a random empty row
+            int rndMove = 0;
+            Random rnd = new Random();
+            if (prepForkYes)
+            {
+                rndMove = rnd.Next(prepFork.Length);
+                while (!currentBoard.validBlank(prepFork[rndMove]))
+                {
+                    rndMove = rnd.Next(prepFork.Length);
+                }
+                gameLog += "h";
+                return prepFork[rndMove];
+            }
+
+            //else make a move set up a line
+            if(setupForkYes && currentBoard.validBlank(setupFork[0]))
+            {
+                gameLog += "i";
+                return setupFork[0];
+            }
+
+            // else go in any available space
+            while (!currentBoard.validBlank(rndMove))
+            {
+             rndMove = rnd.Next(currentBoard.Size * currentBoard.Size)+1;
+            }
+            gameLog += "j";
+            return rndMove;
+        }
+
+        static void SaveLog()
+        {
+            if (!twoPlayer)
+            {
+                //string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\TicTacToeLog.txt";
+                string filePath = "TicTacToeLog.txt";
+                System.IO.StreamWriter logWriter = new System.IO.StreamWriter(filePath, System.IO.File.Exists(filePath));
+                logWriter.WriteLine(gameLog);
+                logWriter.Close();
+                gameLog = "v1" + DateTime.Now.ToString();
+            }
+        }
+    }
+}
